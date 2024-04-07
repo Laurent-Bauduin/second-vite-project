ad892834e764f6afa28469e176c525136cbe43b8
Laurent-Bauduin <laurent.bauduin@outlook.com>
2024-04-07 21:18:28 +0200

    buttons display under conditions

--- src/App.jsx
+++ src/App.jsx
@@ -30,22 +30,20 @@ const pokemonList = [
 function App() {
   let [pokemonIndex, setPokemonIndex] = useState(0);
 
-  const handleClickPrevious = () => {
-    setPokemonIndex((pokemonIndex -= 1));
-    console.log(pokemonIndex);
-  };
-
-  const handleClickNext = () => {
-    setPokemonIndex((pokemonIndex += 1));
-    console.log(pokemonIndex);
-  };
-
   return (
-    <div>
+    <>
       <PokemonCard pokemon={pokemonList[pokemonIndex]} />
-      <button onClick={handleClickPrevious}>Précédent</button>
-      <button onClick={handleClickNext}>Suivant</button>
-    </div>
+      {pokemonIndex > 0 && (
+        <button onClick={() => setPokemonIndex(pokemonIndex - 1)}>
+          Précédent
+        </button>
+      )}
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button onClick={() => setPokemonIndex(pokemonIndex + 1)}>
+          Suivant
+        </button>
+      )}
+    </>
   );
 }
 
